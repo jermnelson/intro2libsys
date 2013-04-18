6d1400ac7766655e25c056add8f3e7e312445c89
Jeremy Nelson <jermnelson.gmail.com>
2013-04-18 16:56:05 -0600

    Added two funcs for json ld management of resources

---
+++ intro2libsys/utilities.py
@@ -0,0 +1,18 @@
+def add_author(given, family, birth=None, death=None, idloc=None):
+    author = {'givenName':given,
+              'familyName':family}
+    if birth is not None:
+        author['birthDate'] = birth
+    if death is not None:
+        author['deathDate'] = death
+    if idloc is not None:
+        author['idloc:uri'] = idloc
+    return author
+
+def creative_work(type_of=None):
+    doc = {"@context": {"@vocab": "http://schema.org/",
+                        "idloc": "http://id.loc.gov"}}
+    if type_of is not None:
+        doc['@type'] = type_of
+    return doc
+
