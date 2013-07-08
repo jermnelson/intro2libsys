a283b4bfa7906c3752265e3471a185e886bc46eb
Jeremy Nelson <jermnelson@gmail.com>
2013-07-08 12:03:15 -0600

    Added topic js

---
+++ intro2libsys/topics/assets/js/topics.js
@@ -0,0 +1,13 @@
+function AgileDevelopmentViewModel() {
+  var self = this;
+
+  self.pageQuiz = function() {
+    alert("In page quiz");
+  }
+
+  self.topicQuiz = function() {
+    alert("In topic quiz");
+ 
+  }
+
+}
