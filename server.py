1d8fda40a5ae6e17826ca9f7eec5777a21983898
Jeremy Nelson <jermnelson@gmail.com>
2013-12-15 21:36:39 -0700

    Started website for intro2libsys and new vita

--- server.py
+++ server.py
@@ -6,10 +6,32 @@ __copyright__ = '(c) 2012-2014 by Jeremy Nelson'
 
 import json
 import os
+
 from flask import Flask, g, jsonify, redirect, render_template
 
 app = Flask(__name__)
 
+
+@app.route("/JeremyNelson/<md_page>.html")
+def JeremyNelson(md_page):
+    markdown = None
+    with open(os.path.join('static',
+                            'md',
+                            '{0}.md'.format(md_page))) as raw_md:
+        markdown = raw_md.read()
+    return render_template('markdown.html',
+                           markdown=markdown)
+
+@app.route('/topics')
+def topics():
+    return render_template('topics.html',
+                           page='topics',
+                           topics={})
+
+@app.route('/<page>')
+def page_router(page):
+    return render_template('{0}.html'.format(page),
+                           page=page)
 
 @app.route('/')
 def index():
